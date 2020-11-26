6e9c8bad2214bb10cd350a235765b0834587d4ec
Dr.jacky <dr.jacky.2005@gmail.com>
2020-11-26 19:51:41 +0000

    Fix non androidx libraries

--- buildSrc/src/main/kotlin/app/web/drjackycv/buildsrc/Depends.kt
+++ buildSrc/src/main/kotlin/app/web/drjackycv/buildsrc/Depends.kt
@@ -28,6 +28,7 @@ object Depends {
         const val coreKtxVersion = "1.3.2"
         const val navigationVersion = "2.3.2"
         const val pagingVersion = "3.0.0-alpha10"//"2.1.2"
+        const val composeNavigationVersion = "1.0.0-alpha02"
         const val multidexVersion = "2.0.1"
         const val fragmentExtVersion = "1.2.5"
         const val recyclerviewVersion = "1.2.0-alpha06"
@@ -158,6 +159,8 @@ object Depends {
         const val compose_material = "androidx.compose.material:material:${Versions.composeVersion}"
         const val compose_runtime_saved_instance_state =
             "androidx.compose.runtime:runtime-saved-instance-state:${Versions.composeVersion}"
+        const val compose_navigation =
+            "androidx.navigation:navigation-compose:${Versions.composeNavigationVersion}"
         const val ui_test = "androidx.ui:ui-test:${Versions.composeVersion}"
         const val ui_tooling = "androidx.ui:ui-tooling:${Versions.composeVersion}"
     }
